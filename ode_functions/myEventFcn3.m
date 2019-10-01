99dc5eb80fe5251b38ef1a8ff5661c7517577deb
Rick Waasdorp <rickwaasdorp@hotmail.com>
2019-10-01 23:16:55 +0200

    update ode event

--- ode_functions/myEventFcn3.m
+++ ode_functions/myEventFcn3.m
@@ -1,4 +1,18 @@
-function [value, isterminal, direction] = myEventFcn3(t, y, maxTimeOut, abortFile)
+function [value, isterminal, direction] = myEventFcn3(t, y, maxTimeOut, abortFile, tspan)
+
+    % determine number of iterations per time bin
+    persistent iterations_per_bin
+
+    if isempty(iterations_per_bin)
+        iterations_per_bin = zeros(size(tspan));
+    end
+
+    bin_num = find(t <= tspan, 1, 'first');
+    iterations_per_bin(bin_num) = iterations_per_bin(bin_num) + 1;
+
+    if bin_num > numel(tspan) - 1
+        assignin('base', 'iterations_per_bin', iterations_per_bin)
+    end
 
     % make dependent on last update time abort/log file
     f = dir(abortFile);
